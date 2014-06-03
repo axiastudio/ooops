6d30d5f4e6845a9d2b1c1aed2a865000a65b10d7
Tiziano Lattisi <tiziano@axiastudio.it>
2014-06-03 21:19:56 +0200

    1st commit

---
+++ src/com/axiastudio/ooops/filters/Filter.java
@@ -0,0 +1,10 @@
+package com.axiastudio.ooops.filters;
+
+/**
+ * User: tiziano
+ * Date: 03/06/14
+ * Time: 16:59
+ */
+public enum Filter {
+    writer8, writer_pdf_Export
+}
---
+++ test/com/axiastudio/ooops/OoopsTest.java
@@ -0,0 +1,29 @@
+package com.axiastudio.ooops;
+
+import com.axiastudio.ooops.filters.Filter;
+import org.junit.Test;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.*;
+
+public class OoopsTest {
+
+    @Test
+    public void test() throws Exception {
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("name", "Tiziano");
+
+        Ooops.create()
+                .open("uno:socket,host=localhost,port=8100;urp;StarOffice.ServiceManager")
+                .load(new FileInputStream(new File("test.odt")))
+                .fromMap(map)
+                .filter(Filter.writer_pdf_Export)
+                .toStream(new FileOutputStream(new File("test.pdf")));
+
+    }
+
+}
